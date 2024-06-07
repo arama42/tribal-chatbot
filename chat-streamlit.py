354092b89fd92b0724ef2dae70fcbeedc1398b02
Aparna Ramachandran <114880737+arama42@users.noreply.github.com>
2024-06-06 23:07:05 -0700

    Update chat

--- chat-streamlit.py
+++ chat-streamlit.py
@@ -29,6 +29,18 @@ def app():
         return "", chat_history
 
     # --------------------------- interface --------------------------------#
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
     with st.sidebar:
         url = "https://sociology.northwestern.edu/people/faculty/core/beth-redbird-.html"
         st.title("[Redbird Lab](%s)" % url)
